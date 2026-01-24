8707832ec1fc67a1f819717e4f419ca36ba8b3f0
joelhmartin <joelhmartin@gmail.com>
2026-01-23 22:07:42 -0600

    2.9.14

--- anchor-private-file-manager.php
+++ anchor-private-file-manager.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Anchor Private File Manager
  * Description: Secure, modern private file manager with folders, role permissions, previews, and logging.
- * Version: 2.9.13
+ * Version: 2.9.14
  * Author: Anchor Corps
  */
 
